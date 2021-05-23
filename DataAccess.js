fe601608a9e502c100886933adc720cc2faab557
Ryan Eakins <ryanmeakins@gmail.com>
2021-05-23 15:41:40 -0700

    mongo port 8080

--- DataAccess.js
+++ DataAccess.js
@@ -16,7 +16,7 @@ var DataAccess = /** @class */ (function () {
         this.mongooseInstance = Mongoose.connect(this.DB_CONNECTION_STRING);
         return this.mongooseInstance;
     };
-    DataAccess.DB_CONNECTION_STRING = 'mongodb://dbAdmin:test@localhost:3000/propertySample?authSource=admin';
+    DataAccess.DB_CONNECTION_STRING = 'mongodb://dbAdmin:test@localhost:8080/propertySample?authSource=admin';
     return DataAccess;
 }());
 exports.DataAccess = DataAccess;
